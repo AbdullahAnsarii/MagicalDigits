b087a793db9ade927f78d9a7f9b5bb04e4950ee1
Abdullah Ansari <aa9272137@gmail.com>
2022-05-07 03:05:05 +0500

    SearchComponent added to jobs

--- pages/careers.jsx
+++ pages/careers.jsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Careers.module.scss'
-import { Container, Box, Grid, List, Divider, ListItem, ListItemText, ListItemIcon, Button, Paper, Chip } from '@mui/material'
+import { Container, Box, Grid, List, Divider, ListItem, ListItemText, ListItemIcon, Button, Paper, Chip, TextField } from '@mui/material'
 import { BrowserView, isDesktop, isMobile, MobileView } from 'react-device-detect';
 import { useEffect, useLayoutEffect, useState } from 'react';
 import PaidIcon from '@mui/icons-material/Paid';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-import CoffeeIcon from '@mui/icons-material/Coffee';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import AutoGraphIcon from '@mui/icons-material/AutoGraph';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import GroupsIcon from '@mui/icons-material/Groups';
@@ -15,13 +15,36 @@ import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 import MoneyIcon from '@mui/icons-material/Money';
 import WorkIcon from '@mui/icons-material/Work';
 
-const Careers = () => {
+export const getStaticProps = () => {
+    const jobs = [
+        { title: 'Junior Software Engineer', closes: '2022-05-15', positions: 1 },
+        { title: 'Associate Software Engineer', closes: '2022-06-30', positions: 4 },
+        { title: 'Junior Mobile Developer', closes: '2022-05-25', positions: 1 },
+        { title: 'Associate Test Engineer', closes: '2022-04-18', positions: 2 }
+    ]
+
+    return {
+        props: { jobs }
+    }
+
+}
+
+const Careers = ({ jobs }) => {
     const [source, setSource] = useState('');
-    const [flexDirectionResponsive, setFlexDirectionResponsive] = useState('row');
+    const [searchText, setSearchText] = useState('');
+    const [showResult, setShowResult] = useState(jobs);
+
     useLayoutEffect(() => {
         isMobile ? setSource('/careersmobile.png') : setSource('/careersdesktop.png');
-        isMobile ? setFlexDirectionResponsive('column') : setFlexDirectionResponsive('row');
     }, [])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setShowResult(jobs.filter(job => {
+            return job.title.toLowerCase().includes(searchText.toLowerCase());
+        }))
+    }
+
     return (
         <>
             <img src={source} width="100%" />
@@ -117,10 +140,10 @@ const Careers = () => {
 
                             <ListItem>
                                 <ListItemIcon>
-                                    <CoffeeIcon fontSize='large' color='primary' />
+                                    <SportsEsportsIcon fontSize='large' color='primary' />
                                 </ListItemIcon>
 
-                                <ListItemText primary='Free unlimited tea, coffee and snacks among other things.' />
+                                <ListItemText primary='Recreational activities to boost productivity.' />
                             </ListItem>
 
                             <Divider variant="inset" component="li" />
@@ -157,63 +180,40 @@ const Careers = () => {
                 </Grid>
 
                 <h6 className={styles.heading2}>Current Job Openings.</h6>
-                <Box id='jobopenings' sx={{ display: 'flex', justifyContent: 'space-evenly', flexDirection: flexDirectionResponsive }}>
-                    <Paper elevation={4} sx={{ p: 3, mb:2 }}>
-                        <h3>Junior Software Engineer</h3>
-                        <Chip size="small" label="1 Position" color='secondary' />
-                        <p>Karachi, <img src='https://flagcdn.com/w20/pk.png' /></p>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}><WorkIcon fontSize='small' color='primary' />&nbsp;<span>Permanent</span></Box>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}><AccessTimeIcon fontSize='small' color='primary' />&nbsp;<span>Applications closing on May 15, 2022</span></Box>
-                        <Box sx={{ textAlign: 'center' }}><Button
-                        size='small'
-                            key='Contact us'
-                            href='#jobopenings'
-                            variant="outlined"
-                            color="secondary"
-                            className={styles.animatedButton}
-                            sx={{mt:1}}
-                        >
-                            View Details
-                        </Button></Box>
-                    </Paper>
-                    <Paper elevation={4} sx={{ p: 3, mb:2 }}>
-                        <h3>Associate Software Engineer</h3>
-                        <Chip size="small" label="4 Positions" color='secondary' />
-                        <p>Karachi, <img src='https://flagcdn.com/w20/pk.png' /></p>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}><WorkIcon fontSize='small' color='primary' />&nbsp;<span>Permanent</span></Box>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}><AccessTimeIcon fontSize='small' color='primary' />&nbsp;<span>Applications closing on June 30, 2022</span></Box>
-                        <Box sx={{ textAlign: 'center' }}><Button
-                        size='small'
-                            key='Contact us'
-                            href='#jobopenings'
-                            variant="outlined"
-                            color="secondary"
-                            className={styles.animatedButton}
-                            sx={{mt:1}}
-                        >
-                            View Details
-                        </Button></Box>
-                    </Paper>
-                    <Paper elevation={4} sx={{ p: 3, mb:2 }}>
-                        <h3>Junior Mobile Developer</h3>
-                        <Chip size="small" label="1 Position" color='secondary' />
-                        <p>Karachi, <img src='https://flagcdn.com/w20/pk.png' /></p>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}><WorkIcon fontSize='small' color='primary' />&nbsp;<span>Permanent</span></Box>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}><AccessTimeIcon fontSize='small' color='primary' />&nbsp;<span>Applications closing on May 25, 2022</span></Box>
-                        <Box sx={{ textAlign: 'center' }}><Button
-                        size='small'
-                            key='Contact us'
-                            href='#jobopenings'
-                            variant="outlined"
-                            color="secondary"
-                            className={styles.animatedButton}
-                            sx={{mt:1}}
-                        >
-                            View Details
-                        </Button></Box>
-                    </Paper>
-                </Box>
+                <form onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <TextField sx={{ m: 2.5, }} color='secondary' label='Search Jobs' value={searchText} onChange={e => setSearchText(e.target.value)} variant='standard' size='small' />
+                    <Button
+                        type="submit"
+                        variant="outlined"
+                        color="secondary"
+                        className={styles.animatedButton}>Submit</Button>
 
+                </form>
+                <Grid container sx={{ my: 2 }} id='jobopenings'>
+
+                    {showResult.length > 0 ? showResult.map((job, index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4}>
+                            <Paper elevation={4} sx={{ p: 2, m: 1 }}>
+                                <h3>{job.title}</h3>
+                                <Chip size="small" label={`${job.positions} ${job.positions > 1 ? 'Positions' : 'Position'}`} color='secondary' />
+                                <p>Karachi, <img src='https://flagcdn.com/w20/pk.png' /></p>
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}><WorkIcon fontSize='small' color='primary' />&nbsp;<span>Permanent</span></Box>
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}><AccessTimeIcon fontSize='small' color='primary' />&nbsp;<span>Applications closing on {job.closes}</span></Box>
+                                <Box sx={{ textAlign: 'center' }}><Button
+                                    size='small'
+                                    key='Contact us'
+                                    href='#jobopenings'
+                                    variant="outlined"
+                                    color="secondary"
+                                    className={styles.animatedButton}
+                                    sx={{ mt: 1 }}
+                                >
+                                    View Details
+                                </Button></Box>
+                            </Paper>
+                        </Grid>
+                    )): <h6 className={styles.description}>No jobs found for this keyword.</h6>}
+                </Grid>
 
             </Container>
         </>
